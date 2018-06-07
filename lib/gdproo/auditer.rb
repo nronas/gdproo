bc722e30440db614ce53b9295af05554757a5a6e
John Tzikas <nronas@gmail.com>
2018-06-07 16:10:06 +0100

    Generate lines on non leaf nodes and do not generate lines for empty values

--- lib/gdproo/auditer.rb
+++ lib/gdproo/auditer.rb
@@ -14,21 +14,23 @@ module Gdproo
       tree = build_tree(id)
 
       tree.dfs do |node|
-        if node.children.empty?
-          @lines += node.fields.map do |field|
+        if node.children.empty? || node.fields.present?
+          @lines += node.fields.inject([]) do |res, field|
+            next res if node.resource.send(field[:accessor]).blank?
+
             if node.skipped?
-              "#{node.prefix}#{field[:name]},#{node.resource.send(field[:accessor])},#{field[:description]}"
+              res << "#{node.prefix}#{field[:name]},#{node.resource.send(field[:accessor])},#{field[:description]}"
             else
-              "#{node.prefix}#{node.name.downcase}.#{node.resource.id}.#{field[:name]},#{node.resource.send(field[:accessor])},#{field[:description]}"
+              res << "#{node.prefix}#{node.name.downcase}.#{node.resource.id}.#{field[:name]},#{node.resource.send(field[:accessor])},#{field[:description]}"
             end
           end
-        else
-          node.children.each do |child|
-            if node.skipped?
-              child.prefix += node.prefix
-            else
-              child.prefix += "#{node.name.downcase}.#{node.resource.id}."
-            end
+        end
+
+        node.children.each do |child|
+          if node.skipped?
+            child.prefix += node.prefix
+          else
+            child.prefix += "#{node.name.downcase}.#{node.resource.id}."
           end
         end
       end
